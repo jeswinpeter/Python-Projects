1a1457128b1ba52a674895c4c9dbd0f931af5789
jeswin <jeswinpeter92@gmail.com>
2024-09-09 21:58:31 +0530

    ~~ Revisiting Python ~~

---
+++ First.py
@@ -0,0 +1,12 @@
+# --- Basic Printing, data types 
+print("---- Learning Python Basics -----")
+    
+name: str = "Hydra"
+age: int = 100
+
+check = True
+PI = 3.14
+
+print(type(PI))
+print(type(check))
+print("Name -" ,name , "Age -" ,age)
