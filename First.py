eea9621400527455f35d7187d8a2cdffd7433376
jeswin <jeswinpeter92@gmail.com>
2024-09-10 08:05:44 +0530

    Trying Formated printing

--- First.py
+++ First.py
@@ -4,9 +4,27 @@ print("---- Learning Python Basics -----")
 name: str = "Hydra"
 age: int = 100
 
+name2 = "Tom"
+
+#Trying out Multi line Printing
+comment = """HI there
+    Starting to learn Python"""
+
+#Formated printing with Multi line printing
+mail = """
+Hail {},
+My name is {}
+hail {}
+"""
+
+print(mail.format(name,name2,name))
+
 check = True
 PI = 3.14
 
 print(type(PI))
 print(type(check))
 print("Name -" ,name , "Age -" ,age)
+print(comment)
+
+
