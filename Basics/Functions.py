1095ec06096ef965a5c2d7d680c6467de28812d9
jeswin <jeswinpeter92@gmail.com>
2024-09-15 14:33:46 +0530

    Learning Functions

--- Basics/Functions.py
+++ Basics/Functions.py
@@ -6,3 +6,11 @@ def Hello_world():
 
 Hello_world()
 
+# Exercise 2: Add Two Numbers
+# Write a function that takes two arguments, a and b, and returns their sum.
+
+def add(a = 5, b = 10):
+    print(a + b)
+
+add()
+add(3,2)
