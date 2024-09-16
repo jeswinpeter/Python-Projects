871a03a061c35fed2fd4d7eb31375dd307953ef9
jeswin <jeswinpeter92@gmail.com>
2024-09-16 14:39:19 +0530

    Expirimenting with write mode

--- Basics/Files.py
+++ Basics/Files.py
@@ -1,8 +1,16 @@
 # Opening and Reading from a file
 
-file = open('D:\Text_file.txt', 'r')
+file = open('D:/Text_file.txt', 'r')
 
 comment = file.read()
 print(comment)
 
 file.close()
+
+# trying out write mode
+
+with open('D:/Text_file.txt', 'w') as file:
+    file.write('Checking write mode')
+
+# with open('D:/Text_file.txt', 'a') as file:
+#     file.append
