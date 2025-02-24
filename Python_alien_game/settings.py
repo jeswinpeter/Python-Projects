f43d3b1a8aa45c22465547a4e43f24c09daa7abe
jeswin <jeswinpeter92@gmail.com>
2025-02-24 14:17:47 +0530

    Refactoring

--- Python_alien_game/settings.py
+++ Python_alien_game/settings.py
@@ -5,6 +5,6 @@ class Settings:
         """Initialize the game's settings."""
 
         # Screen settings 
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.screen_width = 1600
+        self.screen_height = 950
+        self.bg_color = (30, 30, 230)
