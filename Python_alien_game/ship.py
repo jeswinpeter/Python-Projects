f2df7b7a75bb89f5215314f3b8e24d6da4619f8a
jeswin <jeswinpeter92@gmail.com>
2025-02-24 14:16:17 +0530

    down scaled the ship image

--- Python_alien_game/ship.py
+++ Python_alien_game/ship.py
@@ -9,9 +9,13 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
-        self.image = pg.image.load('D:/Python Projeccts/Python_alien_game/Images/Space_ship.bmp')
+        self.img = pg.image.load('D:/Python Projeccts/Python_alien_game/Images/Space_ship.bmp')
+        org_size = self.img.get_size()
+        scaled_size = (org_size[0] // 3, org_size[1] // 3) # Scaling the image
+        self.image = pg.transform.scale(self.img, scaled_size)
         self.rect = self.image.get_rect()
 
+ 
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
