73475208d66e2200b81b75baeae9f611fe286d6f
jeswin <jeswinpeter92@gmail.com>
2025-06-09 13:49:41 +0530

    Added How game ends

--- Python_alien_game/alien_invasion.py
+++ Python_alien_game/alien_invasion.py
@@ -36,9 +36,12 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.stats.game_active:    
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+                
             self._update_screen()
 
     def _check_events(self):
@@ -149,18 +152,21 @@ class AlienInvasion:
     def _ship_hit(self):
         """ Respond to the ship being hit by an alien. """
         # Decrement ship_left.
-        self.stats.ship_left -= 1
+        if self.stats.ship_left > 0:
+            self.stats.ship_left -= 1
 
-        # Get rid of any remaining aliens and bullets.
-        self.aliens.empty()
-        self.bullets.empty()
+            # Get rid of any remaining aliens and bullets.
+            self.aliens.empty()
+            self.bullets.empty()
 
-        # Create a new fleet and center the ship.
-        self._create_fleet()
-        self.ship.center_ship()
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
 
-        # Pause.
-        sleep(0.5)
+            # Pause.
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
 
     def _check_aliens_bottom(self):
         """ Check if any aliens have reacherd the bottom of the screen. """
